a43542aa3555d43abef83cba4923ce221cdfa7d9
Per Persson <md2perpe+github@gmail.com>
2018-04-07 14:03:04 +0000

    Use single push() call
    The method push() allows for several arguments. Therefore we can extract it and almost use it as a block statement.

--- src/extension.ts
+++ src/extension.ts
@@ -3,31 +3,35 @@ import DocumentDecorationManager from "./documentDecorationManager";
 export function activate(context: ExtensionContext) {
     const documentDecorationManager = new DocumentDecorationManager();
 
-    context.subscriptions.push(workspace.onDidChangeConfiguration((event) => {
-        if (event.affectsConfiguration("bracketPairColorizer")) {
-            documentDecorationManager.reset();
-        }
-    }));
+    context.subscriptions.push(
+        
+        workspace.onDidChangeConfiguration((event) => {
+            if (event.affectsConfiguration("bracketPairColorizer")) {
+                documentDecorationManager.reset();
+            }
+        }),
 
-    context.subscriptions.push(window.onDidChangeVisibleTextEditors(() => {
-        documentDecorationManager.updateAllDocuments();
-    }));
+        window.onDidChangeVisibleTextEditors(() => {
+            documentDecorationManager.updateAllDocuments();
+        }),
 
-    context.subscriptions.push(workspace.onDidChangeTextDocument((event) => {
-        documentDecorationManager.onDidChangeTextDocument(event.document, event.contentChanges);
-    }));
+        workspace.onDidChangeTextDocument((event) => {
+            documentDecorationManager.onDidChangeTextDocument(event.document, event.contentChanges);
+        }),
 
-    context.subscriptions.push(workspace.onDidCloseTextDocument((event) => {
-        documentDecorationManager.onDidCloseTextDocument(event);
-    }));
+        workspace.onDidCloseTextDocument((event) => {
+            documentDecorationManager.onDidCloseTextDocument(event);
+        }),
 
-    context.subscriptions.push(workspace.onDidOpenTextDocument((event) => {
-        documentDecorationManager.onDidOpenTextDocument(event);
-    }));
+        workspace.onDidOpenTextDocument((event) => {
+            documentDecorationManager.onDidOpenTextDocument(event);
+        }),
 
-    context.subscriptions.push(window.onDidChangeTextEditorSelection((event) => {
-        documentDecorationManager.onDidChangeSelection(event);
-    }));
+        window.onDidChangeTextEditorSelection((event) => {
+            documentDecorationManager.onDidChangeSelection(event);
+        }),
+        
+    );
 
     documentDecorationManager.reset();
 }
